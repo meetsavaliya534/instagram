713ea1a0444e51fbf9a821a40d751e568ffac0f3
Meet Savaliya <meet.savaliya116206@marwadiuniversity.ac.in>
2024-05-06 13:43:34 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,22 +2,14 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAPGRdPBo0pxzbCLAjXjgIfWrImh4uUzj4",
-//   authDomain: "instagram-ef4dc.firebaseapp.com",
-//   projectId: "instagram-ef4dc",
-//   storageBucket: "instagram-ef4dc.appspot.com",
-//   messagingSenderId: "880988124873",
-//   appId: "1:880988124873:web:89963873e0f0586fef7f18",
-//   measurementId: "G-H1J2NQ2MD1",
-// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAi5Y4AKQ93IkjeAh8_UQtBL5WllKfkmyU",
-  authDomain: "insta-ab30c.firebaseapp.com",
-  projectId: "insta-ab30c",
-  storageBucket: "insta-ab30c.appspot.com",
-  messagingSenderId: "458388709764",
-  appId: "1:458388709764:web:98f7a9d68029e2a0097dd1"
+  apiKey: "YOUR_FIREBASE_APIKEY",
+  authDomain: "AUTHDOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "STORAGEBUCKET",
+  messagingSenderId: "MESSAGINGSENDERID",
+  appId: "YOUR_APP_ID"
 };
 
 // Initialize Firebase
